279a9f4b41df46594e83597609bdbe0c19c4e6b3
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2022-11-16 15:45:42 -0300

    Nosso primeiro programa e o Visual Studio.

---
+++ AprendendoCSharp/OlaMundo no VSCode.cs
@@ -0,0 +1,10 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Olá, mundo");
+        Console.WriteLine("tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
