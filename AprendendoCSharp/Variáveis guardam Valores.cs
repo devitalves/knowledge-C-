3f3d624e1d480cf0b3fa4cee3579faacb9c02dac
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2022-11-18 16:24:04 -0300

    Variáveis de texto.

---
+++ AprendendoCSharp/Variáveis guardam Valores.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 6 - Atribuição de variaveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        
+        Console.WriteLine(idadeAna);
+
+        idade = 25;
+
+        Console.WriteLine("Tecle enter paa fechar...");
+        Console.ReadLine();
+    }
+}
