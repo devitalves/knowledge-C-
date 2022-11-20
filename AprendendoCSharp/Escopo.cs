f92fb64b806b4d20311d9633ea058cd8405cb7d0
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2022-11-20 19:17:39 -0300

    Controle de fluxo com IF.

---
+++ AprendendoCSharp/Escopo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 7 - Condicionais");
+
+
+        int idadeJoao = 16;
+        int quantidadePesssoas = 2;
+
+        bool acompanhado = quantidadePesssoas > 1;
+
+        string textoAdicional;
+
+        if (acompanhado == true)
+        {
+            textoAdicional = "João está acompanhado";
+        }
+        else
+        {
+            textoAdicional = "João não está acompnahdo";
+        }
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine(textoAdicional);
+            Console.WriteLine("Pode entrar!");
+        }
+
+        else
+        {
+            Console.WriteLine("Não pode entrar!");
+        }
+
+        Console.WriteLine("Tecle enter paa fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ AprendendoCSharp/Trabalhando com IF.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 7 - Condicionais");
+
+
+        int idadeJoao = 16;
+        int quantidadePesssoaas = 2;
+
+        if (idadeJoao >= 18)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+            if (quantidadePesssoaas > 0)
+            {
+                Console.WriteLine("Ele está acompnhado, pode entarr!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+
+        }
+
+        Console.WriteLine("Tecle enter paa fechar...");
+        Console.ReadLine();
+    }
+}
