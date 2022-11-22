0043637cea3862afffcb676cc7c7028ac09efb96
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2022-11-22 17:14:31 -0300

    Controle de fluxo com laços de repetição.

---
+++ AprendendoCSharp/A palavra chave break.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 13 - Encadeando For");
+       
+        //*
+        //**
+        //***
+        //****
+        //*****
+
+        //com break
+        for (int contadorLinhas = 0; contadorLinhas < 10; contadorLinhas++)
+        {
+            for (int contadorColunas = 0; contadorColunas < 10; contadorColunas++)
+            {
+                Console.WriteLine("*");
+                if (contadorColunas >= contadorLinhas)
+                    break;
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine("Tecle enter paa fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ AprendendoCSharp/Laço de repetição FOR.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Executando projeto 11 - Calcula Poupança");
+
+        
+        /*
+        while (mes <= 12)
+        {
+            investimento = investimento + investimento * 0.005;
+            Console.WriteLine("No mês " + mes + " Você tem R$ " + investimento);
+
+            mes = mes + 1;
+        }
+        */
+
+        double investimento = 1000;
+
+
+        for (int mes = 1; mes <= 12; mes++)
+       {
+            investimento *= 1.005;
+            Console.WriteLine("No mês " + mes + " Você tem R$ " + investimento);
+        }
+
+        Console.WriteLine("Tecle enter paa fechar...");
+        Console.ReadLine();
+    }
+}
