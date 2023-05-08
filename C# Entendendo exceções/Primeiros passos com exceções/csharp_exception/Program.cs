337fae26a88516ba6c7284cba2b52b289062c373
Ítalo Alves <101436057+devitalves@users.noreply.github.com>
2023-05-08 18:59:39 -0300

    Primeiros passos com exceções

---
+++ C# Entendendo exceções/Primeiros passos com exceções/csharp_exception/Program.cs
@@ -0,0 +1,8 @@
+﻿using csharp_exception.Titular;
+using csharp_exception.Contas;
+
+
+ContaCorrente conta1 = new ContaCorrente(283, "1234-X");
+Console.WriteLine(ContaCorrente.TaxaOperacao);
+Console.ReadLine();
+
